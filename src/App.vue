f0c7b581749f0e61aa731051393f997b4f3f4f1f
themusecatcher <themusecatcher@163.com>
2024-09-20 17:58:22 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,3 @@
 <template>
-  <RouterView class="m-app" />
+  <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 // 使用 storage
 import storage from 'utils/storage'
+// 使用 Time
+import { useTime } from 'hooks/useTime'
 storage.set('token', 'tokennnnn')
+const { year, month, week, day, hour, minute, second } = useTime()
 </script>
 
 <template>
   <main> 你好 </main>
+  <h1>周 {{ week }}</h1>
+  <h1>Date: {{ year }}-{{ month }}-{{ day }} {{ hour }}:{{ minute }}:{{ second }}</h1>
 </template>
