4e997627b10aac28612f17cbb9005bad3eebc883
themusecatcher <themusecatcher@163.com>
2025-08-04 15:56:57 +0800

    feat: update apis

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,18 +1,5 @@
 import { http } from 'http/index'
 import { ContentTypeEnum } from '@/enums/httpEnum'
-export function postForm(url: string, params: object = {}, options: object = {}) {
-  return http.request(
-    {
-      url,
-      method: 'post',
-      data: params,
-      headers: {
-        'Content-Type': ContentTypeEnum.FORM_DATA
-      }
-    },
-    options
-  )
-}
 export function getAction(url: string, params: object = {}, options: object = {}) {
   return http.request(
     {
@@ -27,6 +14,19 @@ export function postAction(url: string, params: object = {}, options: object = {
   return http.request(
     {
       url: url,
+      method: 'post',
+      data: params,
+      headers: {
+        'Content-Type': ContentTypeEnum.FORM_DATA
+      }
+    },
+    options
+  )
+}
+export function postForm(url: string, params: object = {}, options: object = {}) {
+  return http.request(
+    {
+      url,
       method: 'post',
       data: params,
       headers: {
