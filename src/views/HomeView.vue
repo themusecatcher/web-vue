34c0d7b12540cab6501f5bd37a79be6c5dd8ec42
themusecatcher <themusecatcher@163.com>
2025-08-07 11:41:22 +0800

    feat: update homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-onMounted(() => {
-  // window.$message.success('点击了按钮')
-  console.log('message', window.$message)
-})
+function onClick() {
+  window.$message.success('点击了按钮')
+}
 </script>
 <template>
   <div style="margin: 60px 120px">
-    <Button>按钮</Button>
+    <Button @click="onClick">按钮</Button>
   </div>
 </template>
