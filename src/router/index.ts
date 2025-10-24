13e5eb839d32e9a963a743ff0a293577421276d9
themusecatcher <themusecatcher@163.com>
2025-10-24 15:33:11 +0800

    feat: update router

--- src/router/index.ts
+++ src/router/index.ts
@@ -37,15 +37,20 @@ export const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL), // 使用 history 模式，hash 模式：createWebHashHistory
+  /**
+   * 路由器使用的历史记录模式。大多数应用应该使用 createWebHistory，但这需要正确配置服务器。
+   * 你也可以使用 createWebHashHistory 来实现基于 hash 的历史记录，无需配置服务器。但这种方式不会被搜索引擎处理，SEO 的效果较差。
+   */
+  history: createWebHistory(import.meta.env.BASE_URL), // 第一个参数 base: string 基准路径，它被预置到每个 URL 上。这允许在一个域名子文件夹中托管 SPA，例如将 base 设置为 /sub-folder 使得其托管在 example.com/sub-folder。
   routes, // `routes: routes` 的缩写
-  scrollBehavior(to, from, savedPosition) {
+  // scrollBehavior(to, from, savedPosition) {}
+  scrollBehavior() {
     // 滚动行为
     return { left: 0, top: 0, behavior: 'smooth' }
   }
 })
-// 注册全局前置守卫
-router.beforeEach((to, from) => {
+// 注册全局前置守卫 (to, from) => {}
+router.beforeEach((to) => {
   const domTitle = to.meta.title
   const appTitle = import.meta.env.VITE_GLOB_APP_TITLE
   document.title = `${domTitle} - ${appTitle}`
