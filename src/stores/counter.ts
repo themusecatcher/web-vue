ca96fa0529541cdfe0ba4cc5b43f455bc77799dc
themusecatcher <themusecatcher@163.com>
2025-11-18 12:26:25 +0800

    feat: update stores & dependencies

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,6 +1,7 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
-
+//  `defineStore()` 的返回值的命名是自由的
+// 但最好含有 store 的名字，且以 `use` 开头，以 `Store` 结尾。
+// (比如 `useUserStore`，`useCartStore`，`useProductStore`)
+// 第一个参数是你的应用中 Store 的唯一 ID。
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
